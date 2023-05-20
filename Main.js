6ae06dd1c081f65521ad960d3b7e6212d81d3bee
Till <till.wadehn@gmail.com>
2023-05-20 20:11:37 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -9,45 +9,45 @@ const Router = require('./Router');
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
 
-// // Database
-// const db = mysql.createConnection({
-//     host: '34.155.124.6',
-//     user: 'root',
-//     password: 'TillWadehn15.',
-//     database: 'user'
-// });
+// Database
+const db = mysql.createConnection({
+    host: 'dpg-chkfrl64dadfmskdlu6g-a.frankfurt-postgres.render.com',
+    user: 'user',
+    password: 'tFfTZTQdujftoXtTYhQcUMhbYjtV8LRS',
+    database: 'app_v5c8'
+});
 
-// db.connect(function (err) {
-//     console.log('Successful Connection!')
-//     if (err) {
-//         console.log('DB error');
-//         throw err;
-//         return false;
-//     }
-// });
+db.connect(function (err) {
+    console.log('Successful Connection!')
+    if (err) {
+        console.log('DB error');
+        throw err;
+        return false;
+    }
+});
 
-// const sessionStore = new MySQLStore({
-//     expiration: 604800000,
-//     endConnectionOnClose: false
-// }, db);
+const sessionStore = new MySQLStore({
+    expiration: 604800000,
+    endConnectionOnClose: false
+}, db);
 
-// app.use(session({
-//     key: 'dghaoisdvhdvdlfkhs',
-//     secret: 'reikghbldsfjansgbsd',
-//     store: sessionStore,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         maxAge: 604800000,
-//         httpOnly: false
-//     }
-// }));
+app.use(session({
+    key: 'dghaoisdvhdvdlfkhs',
+    secret: 'reikghbldsfjansgbsd',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 604800000,
+        httpOnly: false
+    }
+}));
 
-// new Router(app, db);
+new Router(app, db);
 
-// app.get('/', function(req, res){
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT);
